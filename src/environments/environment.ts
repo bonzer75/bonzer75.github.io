ecf23fb7464c81b96e98e207dc80e6fb931b1dd7
bonzer75 <haroldveracs@gmail.com>
2022-09-19 18:06:41 -0500

    firebase setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'angular-database-f0ce5',
+    appId: '1:1074729407937:web:0496927fe1e727b1c84b40',
+    storageBucket: 'angular-database-f0ce5.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCoi7LlFwnOcOJ25t-4Ck6W-0uf_ZctimM',
+    authDomain: 'angular-database-f0ce5.firebaseapp.com',
+    messagingSenderId: '1074729407937',
+  },
   production: false
 };
 
