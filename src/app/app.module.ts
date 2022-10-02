1ae75c13c1c3c98e09185676c7034fc9c70f76d3
bonzer75 <bonzer-noreply@fedora.email>
2022-10-02 17:39:27 -0500

    added ngx translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
+//local created components
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { FormComponent } from './form/form.component';
 import { FooterComponent } from './footer/footer.component';
+//downloaded modules
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { LayoutModule } from '@angular/cdk/layout';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+//angular firebase
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+//agunar translate
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 @NgModule({
   declarations: [
@@ -26,6 +31,13 @@ import { provideFirestore,getFirestore } from '@angular/fire/firestore';
   ],
   imports: [
     BrowserModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpFactory,
+        deps: [HttpClient]
+      }
+    }),
     FontAwesomeModule,
     LayoutModule,
     ReactiveFormsModule,
@@ -37,3 +49,7 @@ import { provideFirestore,getFirestore } from '@angular/fire/firestore';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function HttpFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http)
+}
